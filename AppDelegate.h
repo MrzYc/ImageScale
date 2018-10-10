83b1fab7b4505df712817304f4f3c00fb314d7e6
zhYch <2751335334@qq.com>
2018-10-10 16:48:08 +0800

    Initial commit

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageScale
+//
+//  Created by zhYch on 2018/10/9.
+//  Copyright © 2018年 zhaoyongchuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageScale
+//
+//  Created by zhYch on 2018/10/9.
+//  Copyright © 2018年 zhaoyongchuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
